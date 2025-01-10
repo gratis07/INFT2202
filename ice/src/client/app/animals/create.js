db6430901265df5f80d09f47eaecf55236905993
Gratis Jacob <gratis.jacob@dcmail.ca>
2025-01-10 14:28:33 -0500

    ICE2

---
+++ ice/src/client/app/animals/create.js
@@ -0,0 +1,7 @@
+/*
+    Name: Gratis Jacob
+    filename: create.js
+    Course: INFT 2202
+    Date: December 31, 1999
+    Description: This is my general application script.  Functions that are required on every page should live here.
+*/
---
+++ ice/src/client/app/animals/search.js
@@ -0,0 +1,6 @@
+/*
+    Name: Gratis Jacob
+    filename: search.js
+    Course: INFT 2202
+    Date: December 31, 1999
+    Description: This is my search script. */
